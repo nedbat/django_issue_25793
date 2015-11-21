09dbc3c7d1f9bd5e7b74450fead0de811c98211d
Ned Batchelder <ned@nedbatchelder.com>
2015-11-21 09:23:10 -0500

    Created the MTI models

--- README.txt
+++ README.txt
@@ -2,3 +2,5 @@ pip install -r requirements.pip
 django-admin startproject djangomti
 cd djangomti
 python manage.py startapp mti
+# ... edit mti/models.py ...
+
--- djangomti/mti/models.py
+++ djangomti/mti/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class ArticlePlugin(models.Model):
+    deleted = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+
+class Subscription(models.Model):
+    subscription_id = models.AutoField(primary_key=True)
+    send_emails = models.BooleanField(default=True)
+
+
+class ArticleSubscription(ArticlePlugin, Subscription):
+    def __unicode__(self):
+        return u"Subscription!"
